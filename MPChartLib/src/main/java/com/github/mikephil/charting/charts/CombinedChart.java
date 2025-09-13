e7cce5915ef8cd028bd1a5d97b80dafae11e65c6
Hannes Achleitner <hannes.software@gmx.at>
2025-09-13 18:07:30 +0200

    Code format

--- MPChartLib/src/main/java/com/github/mikephil/charting/charts/CombinedChart.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/charts/CombinedChart.java
@@ -261,16 +261,16 @@ public class CombinedChart extends BarLineChartBase<CombinedData> implements Com
 			}
 
 			// callbacks to update the content
-			IMarker markerItem =  mMarkers.get(i % mMarkers.size());
-            markerItem.refreshContent(e, highlight);
+			IMarker markerItem = mMarkers.get(i % mMarkers.size());
+			markerItem.refreshContent(e, highlight);
 
 			// draw the marker
 			markerItem.draw(canvas, pos[0], pos[1]);
 		}
 	}
 
-    @Override
-    public String getAccessibilityDescription() {
-        return "This is a combined chart";
-    }
+	@Override
+	public String getAccessibilityDescription() {
+		return "This is a combined chart";
+	}
 }
