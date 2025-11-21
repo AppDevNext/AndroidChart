0a5cebefd9b3249b64cfd28c7b40c43cda232900
Hannes Achleitner <hannes.software@gmx.at>
2025-11-21 09:25:50 +0100

    Build with kts

--- MPChartLib/build.gradle.kts
+++ MPChartLib/build.gradle.kts
@@ -1,60 +1,63 @@
+import info.git.versionHelper.getVersionText
+import java.net.URI
+
 plugins {
-    id "com.android.library"
-    id "maven-publish"
-    id "kotlin-android"
-    id "com.vanniktech.maven.publish" version "0.34.0"
+    id("com.android.library")
+    id("maven-publish")
+    id("kotlin-android")
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 android {
-    namespace "com.github.mikephil.charting"
+    namespace = "com.github.mikephil.charting"
     defaultConfig {
-        minSdkVersion 21
-        compileSdk 35
-        targetSdkVersion 35
+        minSdk = 21
+        compileSdk = 35
 
         // VERSION_NAME no longer available as of 4.1
         // https://issuetracker.google.com/issues/158695880
-        buildConfigField "String", "VERSION_NAME", "\"${getTag()}\""
+        buildConfigField("String", "VERSION_NAME", "\"${getVersionText()}\"")
 
-        consumerProguardFiles "proguard-lib.pro"
+        consumerProguardFiles.add(File("proguard-lib.pro"))
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildTypes {
         release {
-            minifyEnabled false
+            isMinifyEnabled = false
         }
     }
     buildFeatures {
         buildConfig = true
     }
     testOptions {
-        unitTests.returnDefaultValues = true // this prevents "not mocked" error
+        unitTests.isReturnDefaultValues = true // this prevents "not mocked" error
     }
 }
 
 dependencies {
-    implementation "androidx.annotation:annotation:1.9.1"
-    implementation "androidx.core:core:1.16.0"
-    implementation "androidx.activity:activity-ktx:1.10.1"
-    testImplementation "junit:junit:4.13.2"
+    implementation("androidx.annotation:annotation:1.9.1")
+    implementation("androidx.core:core:1.16.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    testImplementation("junit:junit:4.13.2")
 }
 
-tasks.register("androidSourcesJar", Jar) {
+tasks.register<Jar>("androidSourcesJar") {
     archiveClassifier.set("sources")
-    from android.sourceSets.main.java.srcDirs
+    from(android.sourceSets["main"].java.srcDirs)
 }
 
 group = "info.mxtracks"
-version = "${getTag()}"
-println "Build version $version"
+var versionVersion = getVersionText()
+println("Build version $versionVersion")
 
 mavenPublishing {
     pom {
         name = "Android Chart"
-        description = "A powerful Android chart view / graph view library, supporting line- bar- pie- radar- bubble- and candlestick charts as well as scaling, dragging and animations"
+        description =
+            "A powerful Android chart view/graph view library, supporting line- bar- pie- radar- bubble- and candlestick charts as well as scaling, dragging and animations"
         inceptionYear = "2022"
         url = "https://github.com/AppDevNext/AndroidChart/"
         licenses {
@@ -81,9 +84,9 @@ mavenPublishing {
     // Github packages
     repositories {
         maven {
-            version = version + "-SNAPSHOT"
+            version = "$versionVersion-SNAPSHOT"
             name = "GitHubPackages"
-            url = "https://maven.pkg.github.com/AppDevNext/AndroidChart"
+            url = URI("https://maven.pkg.github.com/AppDevNext/AndroidChart")
             credentials {
                 username = System.getenv("GITHUBACTOR")
                 password = System.getenv("GITHUBTOKEN")
