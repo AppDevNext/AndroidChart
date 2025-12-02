ddee945f1b75693e333983e0ecd48db30649bdce
Hannes Achleitner <hannes.software@gmx.at>
2025-12-02 07:28:34 +0100

    Kotlin fragments

---
+++ app/src/main/java/info/appdev/chartexample/fragments/SineCosineFragment.kt
@@ -0,0 +1,50 @@
+package info.appdev.chartexample.fragments
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.LineChart
+import info.appdev.chartexample.R
+
+class SineCosineFragment : SimpleFragment() {
+    private var chart: LineChart? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val v = inflater.inflate(R.layout.frag_simple_line, container, false)
+
+        chart = v.findViewById(R.id.lineChart1)
+
+        chart!!.description.isEnabled = false
+
+        chart!!.setDrawGridBackground(false)
+
+        chart!!.setData(generateLineData())
+        chart!!.animateX(3000)
+
+        val tf = Typeface.createFromAsset(requireContext().assets, "OpenSans-Light.ttf")
+
+        val l = chart!!.legend
+        l.typeface = tf
+
+        val leftAxis = chart!!.axisLeft
+        leftAxis.typeface = tf
+        leftAxis.setAxisMaximum(1.2f)
+        leftAxis.setAxisMinimum(-1.2f)
+
+        chart!!.axisRight.isEnabled = false
+
+        val xAxis = chart!!.xAxis
+        xAxis.isEnabled = false
+
+        return v
+    }
+
+    companion object {
+        fun newInstance(): Fragment {
+            return SineCosineFragment()
+        }
+    }
+}
