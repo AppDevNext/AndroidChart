00488d0d495e1ba680dbf28c6fd326b6ee471bb8
Hannes Achleitner <hannes.software@gmx.at>
2025-12-14 11:44:06 +0100

    No index in sample

--- app/src/main/kotlin/info/appdev/chartexample/notimportant/MainActivity.kt
+++ app/src/main/kotlin/info/appdev/chartexample/notimportant/MainActivity.kt
@@ -124,6 +124,7 @@ class MainActivity : ComponentActivity() {
                                 }
                                 startActivity(intent)
                             }
+
                             MenuAction.REPORT -> {
                                 val intent = Intent(
                                     Intent.ACTION_SENDTO,
@@ -150,60 +151,57 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         val menuItems = ArrayList<ContentItem<out DemoBase>>().apply {
-            add(0, ContentItem("Line Charts"))
-            add(0, ContentItem("Line Charts"))
-            add(1, ContentItem("Basic", "Simple line chart.", LineChartActivity::class.java))
-            add(2, ContentItem("Multiple", "Show multiple data sets.", MultiLineChartActivity::class.java))
-            add(3, ContentItem("Dual Axis", "Line chart with dual y-axes.", LineChartDualAxisActivity::class.java))
-            add(4, ContentItem("Inverted Axis", "Inverted y-axis.", InvertedLineChartActivity::class.java))
-            add(5, ContentItem("Cubic", "Line chart with a cubic line shape.", CubicLineChartActivity::class.java))
-            add(6, ContentItem("Colorful", "Colorful line chart.", LineChartActivityColored::class.java))
-            add(7, ContentItem("Performance", "Render 30.000 data points smoothly.", PerformanceLineChart::class.java))
-            add(8, ContentItem("Filled", "Colored area between two lines.", FilledLineActivity::class.java))
+            add(ContentItem("Line Charts"))
+            add(ContentItem("Basic", "Simple line chart.", LineChartActivity::class.java))
+            add(ContentItem("Multiple", "Show multiple data sets.", MultiLineChartActivity::class.java))
+            add(ContentItem("Dual Axis", "Line chart with dual y-axes.", LineChartDualAxisActivity::class.java))
+            add(ContentItem("Inverted Axis", "Inverted y-axis.", InvertedLineChartActivity::class.java))
+            add(ContentItem("Cubic", "Line chart with a cubic line shape.", CubicLineChartActivity::class.java))
+            add(ContentItem("Colorful", "Colorful line chart.", LineChartActivityColored::class.java))
+            add(ContentItem("Performance", "Render 30.000 data points smoothly.", PerformanceLineChart::class.java))
+            add(ContentItem("Filled", "Colored area between two lines.", FilledLineActivity::class.java))
 
-            add(9, ContentItem("Bar Charts"))
-            add(10, ContentItem("Basic", "Simple bar chart.", BarChartActivity::class.java))
-            add(11, ContentItem("Basic 2", "Variation of the simple bar chart.", AnotherBarActivity::class.java))
-            add(12, ContentItem("Multiple", "Show multiple data sets.", BarChartActivityMultiDataset::class.java))
-            add(13, ContentItem("Horizontal", "Render bar chart horizontally.", HorizontalBarChartActivity::class.java))
-            add(14, ContentItem("Stacked", "Stacked bar chart.", StackedBarActivity::class.java))
-            add(15, ContentItem("Negative", "Positive and negative values with unique colors.", BarChartPositiveNegative::class.java))
-            //objects.add(16, ContentItem("Negative Horizontal", "demonstrates how to create a HorizontalBarChart with positive and negative values."))
-            add(17, ContentItem("Stacked 2", "Stacked bar chart with negative values.", StackedBarActivityNegative::class.java))
-            add(18, ContentItem("Sine", "Sine function in bar chart format.", BarChartActivitySinus::class.java))
+            add(ContentItem("Bar Charts"))
+            add(ContentItem("Basic", "Simple bar chart.", BarChartActivity::class.java))
+            add(ContentItem("Basic 2", "Variation of the simple bar chart.", AnotherBarActivity::class.java))
+            add(ContentItem("Multiple", "Show multiple data sets.", BarChartActivityMultiDataset::class.java))
+            add(ContentItem("Horizontal", "Render bar chart horizontally.", HorizontalBarChartActivity::class.java))
+            add(ContentItem("Stacked", "Stacked bar chart.", StackedBarActivity::class.java))
+            add(ContentItem("Negative", "Positive and negative values with unique colors.", BarChartPositiveNegative::class.java))
+            //objects.add(ContentItem("Negative Horizontal", "demonstrates how to create a HorizontalBarChart with positive and negative values."))
+            add(ContentItem("Stacked 2", "Stacked bar chart with negative values.", StackedBarActivityNegative::class.java))
+            add(ContentItem("Sine", "Sine function in bar chart format.", BarChartActivitySinus::class.java))
 
-            add(19, ContentItem("Pie Charts"))
-            add(20, ContentItem("Basic", "Simple pie chart.", PieChartActivity::class.java))
-            add(20, ContentItem("Basic", "Rounded pie chart.", PieChartRoundedActivity::class.java))
-            add(21, ContentItem("Value Lines", "Stylish lines drawn outward from slices.", PiePolylineChartActivity::class.java))
-            add(22, ContentItem("Half Pie", "180° (half) pie chart.", HalfPieChartActivity::class.java))
+            add(ContentItem("Pie Charts"))
+            add(ContentItem("Basic", "Simple pie chart.", PieChartActivity::class.java))
+            add(ContentItem("Basic", "Rounded pie chart.", PieChartRoundedActivity::class.java))
+            add(ContentItem("Value Lines", "Stylish lines drawn outward from slices.", PiePolylineChartActivity::class.java))
+            add(ContentItem("Half Pie", "180° (half) pie chart.", HalfPieChartActivity::class.java))
             add(
-                23,
                 ContentItem(
-                    "Specific positions",
-                    "This demonstrates how to pass a list of specific positions for lines and labels on x and y axis",
+                    "Specific positions", "This demonstrates how to pass a list of specific positions for lines and labels on x and y axis",
                     SpecificPositionsLineChartActivity::class.java
                 )
             )
 
-            add(24, ContentItem("Other Charts"))
-            add(25, ContentItem("Combined Chart", "Bar and line chart together.", CombinedChartActivity::class.java))
-            add(26, ContentItem("Scatter Plot", "Simple scatter plot.", ScatterChartActivity::class.java))
-            add(27, ContentItem("Bubble Chart", "Simple bubble chart.", BubbleChartActivity::class.java))
-            add(28, ContentItem("Candlestick", "Simple financial chart.", CandleStickChartActivity::class.java))
-            add(29, ContentItem("Radar Chart", "Simple web chart.", RadarChartActivity::class.java))
+            add(ContentItem("Other Charts"))
+            add(ContentItem("Combined Chart", "Bar and line chart together.", CombinedChartActivity::class.java))
+            add(ContentItem("Scatter Plot", "Simple scatter plot.", ScatterChartActivity::class.java))
+            add(ContentItem("Bubble Chart", "Simple bubble chart.", BubbleChartActivity::class.java))
+            add(ContentItem("Candlestick", "Simple financial chart.", CandleStickChartActivity::class.java))
+            add(ContentItem("Radar Chart", "Simple web chart.", RadarChartActivity::class.java))
 
-            add(30, ContentItem("Scrolling Charts"))
-            add(31, ContentItem("Multiple", "Various types of charts as fragments.", ListViewMultiChartActivity::class.java))
-            add(32, ContentItem("View Pager", "Swipe through different charts.", ViewPagerSimpleChartDemo::class.java))
-            add(33, ContentItem("Tall Bar Chart", "Bars bigger than your screen!", ScrollViewActivity::class.java))
-            add(34, ContentItem("Many Bar Charts", "More bars than your screen can handle!", ListViewBarChartActivity::class.java))
+            add(ContentItem("Scrolling Charts"))
+            add(ContentItem("Multiple", "Various types of charts as fragments.", ListViewMultiChartActivity::class.java))
+            add(ContentItem("View Pager", "Swipe through different charts.", ViewPagerSimpleChartDemo::class.java))
+            add(ContentItem("Tall Bar Chart", "Bars bigger than your screen!", ScrollViewActivity::class.java))
+            add(ContentItem("Many Bar Charts", "More bars than your screen can handle!", ListViewBarChartActivity::class.java))
 
-            add(35, ContentItem("Even More Line Charts"))
-            add(36, ContentItem("Dynamic", "Build a line chart by adding points and sets.", DynamicalAddingActivity::class.java))
-            add(37, ContentItem("Realtime", "Add data points in realtime.", RealtimeLineChartActivity::class.java))
-            add(38, ContentItem("Hourly", "Uses the current time to add a data point for each hour.", LineChartTimeActivity::class.java))
-            //add(39, new ContentItem("Realm.io Examples", "See more examples that use Realm.io mobile database."));
+            add(ContentItem("Even More Line Charts"))
+            add(ContentItem("Dynamic", "Build a line chart by adding points and sets.", DynamicalAddingActivity::class.java))
+            add(ContentItem("Realtime", "Add data points in realtime.", RealtimeLineChartActivity::class.java))
+            add(ContentItem("Hourly", "Uses the current time to add a data point for each hour.", LineChartTimeActivity::class.java))
+            //add(new ContentItem("Realm.io Examples", "See more examples that use Realm.io mobile database."));
         }
     }
 }
