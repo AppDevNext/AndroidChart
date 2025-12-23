c1e3d52fc9e879fa8a6692532788a9048b3cf593
Hannes Achleitner <hannes.software@gmx.at>
2025-12-23 08:20:50 +0100

    ViewBinding RadarChartActivity

--- app/src/main/kotlin/info/appdev/chartexample/RadarChartActivity.kt
+++ app/src/main/kotlin/info/appdev/chartexample/RadarChartActivity.kt
@@ -156,18 +156,17 @@ class RadarChartActivity : DemoBase() {
             }
 
             R.id.actionToggleValues -> {
-                binding.chart1.data!!.dataSets.forEach {
-                    it?.isDrawValues = !it.isDrawValues
+                binding.chart1.data?.dataSets?.forEach {
+                    it.isDrawValues = !it.isDrawValues
                 }
-
                 binding.chart1.invalidate()
             }
 
             R.id.actionToggleHighlight -> {
-                if (binding.chart1.data != null) {
-                    binding.chart1.data!!.isHighlightEnabled = !binding.chart1.data!!.isHighlightEnabled()
-                    binding.chart1.invalidate()
+                binding.chart1.data?.let {
+                    it.isHighlightEnabled = !it.isHighlightEnabled
                 }
+                binding.chart1.invalidate()
             }
 
             R.id.actionToggleRotate -> {
@@ -176,14 +175,14 @@ class RadarChartActivity : DemoBase() {
             }
 
             R.id.actionToggleFilled -> {
-                binding.chart1.data!!.dataSets.forEach { set ->
-                    set?.setDrawFilled(!set.isDrawFilledEnabled)
+                binding.chart1.data?.dataSets?.forEach { set ->
+                    set.setDrawFilled(!set.isDrawFilledEnabled)
                 }
                 binding.chart1.invalidate()
             }
 
             R.id.actionToggleHighlightCircle -> {
-                binding.chart1.data!!.dataSets.forEach { set ->
+                binding.chart1.data?.dataSets?.forEach { set ->
                     set.isDrawHighlightCircleEnabled = !set.isDrawHighlightCircleEnabled
                 }
                 binding.chart1.invalidate()
