1071684631e1801948cbe1dbd73ed5394e1ab26d
Hannes Achleitner <hannes.software@gmx.at>
2026-01-04 11:13:13 +0100

    Cosmetic

--- chartLib/src/main/kotlin/info/appdev/charting/formatter/DefaultAxisValueFormatter.kt
+++ chartLib/src/main/kotlin/info/appdev/charting/formatter/DefaultAxisValueFormatter.kt
@@ -9,26 +9,20 @@ open class DefaultAxisValueFormatter(digits: Int) : IAxisValueFormatter {
      */
     protected var decimalFormat: DecimalFormat
     /**
-     * Returns the number of decimal digits this formatter uses or -1, if unspecified.
-     */
-    /**
-     * the number of decimal digits this formatter uses
+     * The number of decimal digits this formatter uses or -1, if unspecified.
      */
     var decimalDigits = 0
         protected set
 
-    /**
-     * Constructor that specifies to how many digits the value should be
-     * formatted.
-     */
     init {
         decimalDigits = digits
-        val b = StringBuffer()
+        val stringBuffer = StringBuffer()
         for (i in 0 until digits) {
-            if (i == 0) b.append(".")
-            b.append("0")
+            if (i == 0)
+                stringBuffer.append(".")
+            stringBuffer.append("0")
         }
-        decimalFormat = DecimalFormat("###,###,###,##0$b")
+        decimalFormat = DecimalFormat("###,###,###,##0$stringBuffer")
     }
 
     override fun getFormattedValue(value: Float, axis: AxisBase?): String? {
