70e3d0b78d462be0819797fe84203f3cff6442a1
Hannes Achleitner <hannes.software@gmx.at>
2026-04-03 08:20:33 +0200

    More time value formatter

---
+++ app/src/main/kotlin/info/appdev/chartexample/formatter/UnixTimeValueFormatter.kt
@@ -0,0 +1,38 @@
+package info.appdev.chartexample.formatter
+
+import info.appdev.charting.data.BarEntryDouble
+import info.appdev.charting.data.BarEntryFloat
+import info.appdev.charting.data.EntryFloat
+import info.appdev.charting.formatter.IValueFormatter
+import info.appdev.charting.utils.ViewPortHandler
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class UnixTimeValueFormatter(val format: String = "yyyy-MM-dd'T'HH:mm:ss'Z'") : IValueFormatter {
+
+    val simpleDateFormat = SimpleDateFormat(format, Locale.getDefault())
+
+    // For bar/line values
+    override fun getFormattedValue(value: Float, entryFloat: EntryFloat?, dataSetIndex: Int, viewPortHandler: ViewPortHandler?): String {
+        return when (entryFloat) {
+            is BarEntryDouble if entryFloat.yValsDouble != null -> {
+                // High-precision double path
+                val vals = entryFloat.yValsDouble!!
+                val start = simpleDateFormat.format(vals.first().toLong())
+                val end   = simpleDateFormat.format(vals.last().toLong())
+                "$start - $end"
+            }
+
+            is BarEntryFloat if entryFloat.yVals != null -> {
+                // Float path
+                val vals = entryFloat.yVals!!
+                val start = simpleDateFormat.format(vals.first().toLong())
+                val end   = simpleDateFormat.format(vals.last().toLong())
+                "$start - $end"
+            }
+
+            else -> simpleDateFormat.format(value.toLong())
+        }
+    }
+}
